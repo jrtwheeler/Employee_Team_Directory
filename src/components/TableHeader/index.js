23c49e839dfbecd7a660e8383690ba83d72b0e4f
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-01-18 13:43:33 -0500

    Table Header Component Built

---
+++ src/components/TableHeader/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TableHeader() {
+    return header.map((key, index) => {
+       return <th key={id}>{key.toUpperCase()}</th>
+    })
+ }
+
+ export default TableHeader;
