c55e89b8b66a689a3281b59b184847332c72e088
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-01-18 17:25:33 -0500

    API call retursn 25 users

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,9 @@
 import axios from "axios";
-const BASEURL = "https://randomuser.me/api/?inc=name,email,dob,phone,picture?results=20";
+const BASEURL = "https://randomuser.me/api/?results=25&inc=name,email,dob,phone,picture";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
   getEmployeeDatabase: function() {
-    var showthis = axios.get(BASEURL);
-    console.log(showthis)
-    return showthis
+    return axios.get(BASEURL);
   }
 };
