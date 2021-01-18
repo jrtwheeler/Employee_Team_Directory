57ec65e5a6becd7571fea0b4b061702555870214
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2021-01-17 19:16:35 -0500

    Added table

--- src/App.js
+++ src/App.js
@@ -2,7 +2,41 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Wrapper from "./components/Wrapper";
+import Table from "./components/Table";
 import Directory from "./pages/Directory";
+
+const groceries = [
+  {
+    id: 1,
+    name: "Milk",
+    purchased: true
+  },
+  {
+    id: 2,
+    name: "Eggs",
+    purchased: true
+  },
+  {
+    id: 3,
+    name: "Cheese",
+    purchased: false
+  },
+  {
+    id: 4,
+    name: "Cake Mix",
+    purchased: false
+  },
+  {
+    id: 5,
+    name: "Carrots",
+    purchased: false
+  },
+  {
+    id: 6,
+    name: "Juice",
+    purchased: true
+  }
+];
 
 function App() {
   return (
@@ -12,7 +46,7 @@ function App() {
         <NavBar />
       </div>
       <div>
-        <Directory></Directory>
+        <Table groceries={groceries}></Table>
       </div>
     </Router>
     </Wrapper>
